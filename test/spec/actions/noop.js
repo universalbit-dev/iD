8b827863436b083a6ef01cfee43d724706536f27
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-06-10 22:23:02 +0200

    Delete test directory

--- test/spec/actions/noop.js
+++
@@ -1,7 +0,0 @@
-describe('iD.actionNoop', function () {
-    it('does nothing', function () {
-        var graph = iD.coreGraph(),
-            action = iD.actionNoop(graph);
-        expect(action(graph)).to.equal(graph);
-    });
-});
--- test/spec/ui/inspector.js
+++
@@ -1,2 +0,0 @@
-describe('iD.uiInspector', function () {
-});
