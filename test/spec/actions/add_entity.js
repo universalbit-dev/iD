acb9fa43a00e8364bcd907ce4d17a89a89d61668
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-09-27 16:32:43 +0200

    Delete test directory

--- test/spec/actions/add_entity.js
+++
@@ -1,7 +0,0 @@
-describe('iD.actionAddEntity', function () {
-    it('adds an entity to the graph', function () {
-        var entity = iD.osmEntity(),
-            graph = iD.actionAddEntity(entity)(iD.coreGraph());
-        expect(graph.entity(entity.id)).to.equal(entity);
-    });
-});
