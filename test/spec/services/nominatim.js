e508711111afb79cedfd55b1ffbb3b7a3b65742c
UniversalBit <lazzaragiuliano@gmail.com>
2025-01-22 19:57:28 +0100

    Update nominatim.js
     Copilot Autofix AI generated 
    
    To fix the problem, we need to escape the . meta-character in the regular expression used for mocking the URL. This will ensure that the regular expression matches the intended host (nominatim.openstreetmap.org) accurately and does not allow any unintended hosts to pass the check.
    
        Locate the regular expression on line 141 in the file test/spec/services/nominatim.js.
        Escape the . characters in the regular expression to ensure they match literal dots.

--- test/spec/services/nominatim.js
+++ test/spec/services/nominatim.js
@@ -138,7 +138,7 @@ describe('iD.serviceNominatim', function() {
     describe('#search', function() {
         it('calls the given callback with the results of the search query', function(done) {
             var callback = sinon.spy();
-            fetchMock.mock(new RegExp('https://nominatim.openstreetmap.org/search'), {
+            fetchMock.mock(new RegExp('https://nominatim\\.openstreetmap\\.org/search'), {
                 body: '[{"place_id":"158484588","osm_type":"relation","osm_id":"188022","boundingbox":["39.867005","40.1379593","-75.2802976","-74.9558313"],"lat":"39.9523993","lon":"-75.1635898","display_name":"Philadelphia, Philadelphia County, Pennsylvania, United States of America","class":"place","type":"city","importance":0.83238050437778}]',
                 status: 200,
                 headers: { 'Content-Type': 'application/json' }
